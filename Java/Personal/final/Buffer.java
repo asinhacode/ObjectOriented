f20318ed9f69796c8f688181e135090054e13c1b
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-01-28 20:02:32 -0500

    Add files via upload

---
+++ Java/Personal/final/Buffer.java
@@ -0,0 +1,27 @@
+/*
+ * Buffer are mutable
+ */
+
+package finaL;
+
+public class Buffer {
+	
+
+	public static void main(String[] args) 
+	{
+		StringBuffer name = new StringBuffer("Anupam Sinha");
+		
+		System.out.println(name);
+		name.append('s');
+		
+		System.out.println(name);
+		
+		name.append(32);
+		System.out.println(name);
+		
+		// System.out.println(name.capacity());
+		name.
+		
+
+	}
+}
